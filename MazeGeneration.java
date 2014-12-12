97d05a98099d4790802e177766a63a57ad0af1be
Arn-O <rnd.bertrand@gmail.com>
2014-12-12 16:04:27 +0100

    draft recursive function

--- MazeGeneration.java
+++ MazeGeneration.java
@@ -5,15 +5,62 @@ public class MazeGeneration extends SuperKarel {
 	// You fill in this part
 	
 	public void run() {
+		recMaze();	
+	}
+	
+	public void recMaze() {
 		paintCorner(YELLOW);
-		move();
-		move();
-		turnAround();
+		faceRandom();
+		
+		for (int i = 0; i < 4; i++) {
+			if (frontIsBlocked()) {
+				turnRight();
+			} else {
+				move();
+				if (beepersPresent()) {
+					getBack();
+					turnRight();
+				} else {
+					if (cornerColorIs(YELLOW)) {
+						getBack();
+						turnRight();
+					} else {
+						// unexplored area
+						putBeeperInFront();
+						recMaze();
+					}
+				}
+			}
+		}
+		
+	}
+	
+	public void faceRandom() {
+		
 		for (int i = 0; i < 4; i++) {
 			if (random()) {
 				turnRight();
 			}	
 		}
+		
+	}
+	
+	public void getBack() {
+		
+		turnAround();
+		move();
+		turnAround();
+		
+	}
+	
+	public void putBeeperInFront() {
+		
+		if (frontIsClear()) {
+			move();
+			putBeeper();
+			getBack();
+		}
+		
 	}
 
 }
